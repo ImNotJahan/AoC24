c46d763bbf64f48b30714fa493c155fc325f14b1
Jahan Rashidi <hiimjahan@gmail.com>
2024-12-02 18:42:45 -0800

    Simplified code

--- day2p2.py
+++ day2p2.py
@@ -2,17 +2,9 @@ file = open("input/day2.txt")
 
 goodReportCount = 0
 
-def removeFirstWrong(levels, increasing):
-    for i in range(1, len(levels)):
-        change = levels[i] - levels[i - 1]
-
-        if(change == 0 or (change > 0 and not increasing) or (change < 0 and increasing) or (change > 3 or change < -3)):
-            levels.pop(i - 1)
-            return levels
-
-    return True
-
-def areLevelsValid(levels, increasing):
+def areLevelsValid(levels):
+    increasing = levels[0] - levels[1] < 0
+    
     for i in range(1, len(levels)):
         change = levels[i] - levels[i - 1]
 
@@ -33,19 +25,16 @@ def areLevelsValid(levels, increasing):
 def isGoodReport(report):
     levelsI = report.split()
     levelsI = [int(string) for string in levelsI]
-
-    increasing = levelsI[0] - levelsI[1] < 0
-
-    if(areLevelsValid(levelsI, increasing)):
-       return True
     
+    if(areLevelsValid(levelsI)):
+       return True
+
+    # if is not a good report, check if could be by removing one level (brute forced)
     for i in range(0, len(levelsI)):
         levels = levelsI.copy()
         levels.pop(i)
         
-        increasing = levels[0] - levels[1] < 0
-
-        if(areLevelsValid(levels, increasing)):
+        if(areLevelsValid(levels)):
             return True
 
     return False
